e18883dd225e88cdac3687e2803643e8a969559f
luigi617 <luigiliu617@gmail.com>
2025-01-27 13:44:49 -0500

    deploy

--- config/celery.py
+++ config/celery.py
@@ -2,15 +2,12 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-# Set default Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
 app = Celery('luigiliu-backend')
 
-# Load config from Django settings, the CELERY namespace means all Celery-related config
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Autodiscover tasks across all apps
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
